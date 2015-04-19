83f9308d3517c598bd8a1699b73d11c9b9e0b351
ch-schiffer <ch.schiffer@live.de>
2015-04-19 20:21:07 +0200

    Initial Commit

---
+++ AndroidRemoteController/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace AndroidRemoteController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(447, 677);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(465, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(187, 120);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(531, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(497, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PIN:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(531, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(465, 222);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.button3_PreviewKeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 701);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "AndroidRemoteController";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AndroidRemoteController/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AndroidRemoteController
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<Keys, int> _keyDictionary = new Dictionary<Keys, int>()
+        {
+          {Keys.Tab, 61 },
+          {Keys.Down, 20},
+          {Keys.Up, 19},
+          {Keys.Enter, 66},
+          {Keys.Space, 3},
+          {Keys.Back, 4},
+          {Keys.Left, 21},
+          {Keys.Right,22},
+          {Keys.Escape, 26}
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            Screenshot();
+        }
+
+        public static int Counter
+        {
+            get { return _counter; }
+            set { _counter = value; }
+        }
+
+        private void InputKeyEvent(int key)
+        {
+            string cmd = String.Format("shell input keyevent {0}", key);
+            ExecuteCommand(cmd);
+            Screenshot();
+        }
+
+        private void InputTap(int x, int y)
+        {
+            string cmd = String.Format("shell input tap {0} {1}", x, y);
+            ExecuteCommand(cmd);
+        }
+
+        private void InputText(string text)
+        {
+            string cmd = String.Format("shell input text {0}", text);
+            ExecuteCommand(cmd);
+        }
+
+        private void InputSwipe(int xBefore, int yBefore, int xAfter, int yAfter)
+        {
+            string cmd = String.Format("shell input swipe {0} {1} {2} {3}", xBefore, yBefore, xAfter, yAfter);
+            ExecuteCommand(cmd);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = richTextBox1.Text;
+            InputText(text);
+            Screenshot();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InputKeyEvent(26);
+            InputSwipe(200, 900, 200, 300);
+            string pin = textBox1.Text;
+            InputText(pin);
+            Screenshot();
+        }
+
+        private static int _counter = 0;
+        private void Screenshot()
+        {
+            //if (pictureBox1.Image != null) pictureBox1.Image.Dispose();
+            //pictureBox1.Image = null;
+            ////if (File.Exists("screen"+(_counter-1)+".png")) File.Delete("screen.png");
+            //ExecuteCommand("shell screencap -p /sdcard/screen" + _counter + ".png");
+            //ExecuteCommand("pull /sdcard/screen" + _counter + ".png");
+            //ExecuteCommand("shell rm /sdcard/screen" + _counter + ".png");
+            //if (File.Exists("screen" + _counter + ".png") && (new FileInfo("screen" + _counter + ".png").Length > 0))
+            //{
+            //    pictureBox1.WaitOnLoad = true;
+            //    pictureBox1.LoadAsync("screen" + _counter + ".png");
+            //}
+            //_counter++;
+        }
+
+        private void ExecuteCommand(string cmd)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "adb.exe",
+                Arguments = cmd
+            };
+            process.StartInfo = startInfo;
+            Console.WriteLine("adb {0}", cmd);
+            process.Start();
+            process.WaitForExit();
+        }
+
+       
+        private void button3_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (_keyDictionary.ContainsKey(e.KeyCode))
+
+            {
+                InputKeyEvent(_keyDictionary[e.KeyCode]);
+                e.IsInputKey = true;
+            }
+                
+        }
+    }
+}
